cb6d1e0a389f81536e8248de9baf52be2b42e659
Your Name <harishitasinghmahua@gmail.com>
2024-09-26 13:01:59 +0530

    third time pushed

---
+++ shell8.2.py
@@ -0,0 +1,28 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/haris/Downloads/python day8/day8.4.py
+0           Rupesh Kumar
+1          Diwakar Kumar
+2    Apeksha Shrivastava
+3           Aditya Kumar
+4          Ritika Kumari
+Name: Prince Kumar Sah, dtype: object 0    16.0
+1    15.0
+2    13.0
+3    17.0
+4    11.0
+Name: 14, dtype: float64
+
+============ RESTART: C:/Users/haris/Downloads/python day8/day8.4.py ===========
+131            Pradeep Kumar
+132             Ritesh Kumar
+133    Abhinav Kumar Bhaskar
+134                Rahul Raj
+135             Ahmad Nabeel
+Name: Prince Kumar Sah, dtype: object 131    21.0
+132    17.0
+133    15.0
+134    17.0
+135    19.0
+Name: 14, dtype: float64
